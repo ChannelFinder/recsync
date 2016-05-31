38088e8e319ce297d0ed542f2ccf211c75f90f35
Michael Davidsaver <mdavidsaver@gmail.com>
2016-05-31 14:09:59 -0400

    cfstore is optional.  soft fail when CF client library not present

--- server/twisted/plugins/recceiver_plugin.py
+++ server/twisted/plugins/recceiver_plugin.py
@@ -2,10 +2,17 @@
 
 from recceiver.application import Maker
 
-from recceiver import processors, dbstore, cfstore
+from recceiver import processors, dbstore
 
 serviceMaker = Maker()
 
 showfactory = processors.ProcessorFactory('show', processors.ShowProcessor)
-cffactory = processors.ProcessorFactory('cf', cfstore.CFProcessor)
 dbfactory = processors.ProcessorFactory('db',  dbstore.DBProcessor)
+
+try:
+    from channelfinder import ChannelFinderClient
+except ImportError:
+    pass
+else:
+    from recceiver import cfstore
+    cffactory = processors.ProcessorFactory('cf', cfstore.CFProcessor)
