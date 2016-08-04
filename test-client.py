dbcb95306ed3e594256ab241a0675a98d928ffc5
Michael Davidsaver <mdavidsaver@gmail.com>
2016-08-04 10:48:54 -0400

    add test-client debug utility

---
+++ test-client.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+'''Listen for recceiver Announcements
+'''
+
+import sys, socket
+
+S = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+
+S.bind(('',5049))
+
+print S, S.fileno()
+while True:
+  print '>>', S.recvfrom(1024)
