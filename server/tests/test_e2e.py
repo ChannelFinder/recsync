31125e9c783ffffe8767fccf86e2d2921e94ef21
Sky Brewer <sky.brewer@ess.eu>
2025-02-06 11:33:16 +0100

    Add test logs

--- server/tests/test_e2e.py
+++ server/tests/test_e2e.py
@@ -3,6 +3,15 @@ import time
 from testcontainers.compose import DockerCompose
 
 from channelfinder import ChannelFinderClient
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    encoding="utf-8",
+)
+
+LOG: logging.Logger = logging.getLogger(__name__)
 
 
 def fullSetupDockerCompose() -> DockerCompose:
@@ -16,15 +25,40 @@ def fullSetupDockerCompose() -> DockerCompose:
     )
 
 
-def test_smoke() -> None:
-    """
-    Test that the setup in the docker compose creates channels in channelfinder
-    """
-    with fullSetupDockerCompose() as compose:
-        compose.start()
+class TestE2E:
+    compose: DockerCompose
+
+    def setup_method(self) -> None:
+        """Setup the test environment"""
+        LOG.info("Setting up test")
+        self.compose = fullSetupDockerCompose()
+        self.compose.start()
+
+    def teardown_method(self) -> None:
+        """Teardown the test environment"""
+
+        LOG.info("Tearing down test")
+        if self.compose:
+            LOG.info("Stopping docker compose")
+            LOG.debug(self.compose.get_logs())
+            self.compose.stop()
+
+    def test_smoke(self) -> None:
+        """
+        Test that the setup in the docker compose creates channels in channelfinder
+        """
+        LOG.info("Waiting for channels to sync")
         # wait for channels to sync
-        time.sleep(10)
         cf_client = ChannelFinderClient()
+        for seconds in range(10):
+            try:
+                channels = cf_client.find(name="*")
+                LOG.info("Found %s in %s", len(channels), seconds)
+                if len(channels) == 24:
+                    break
+            except Exception as e:
+                LOG.error(e)
+            time.sleep(1)
         channels = cf_client.find(name="*")
         assert len(channels) == 24
         assert channels[0]["name"] == "IOC1-1::li"
