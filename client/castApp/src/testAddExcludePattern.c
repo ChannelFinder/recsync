8fae20dad569cf184cc584a49f42f80dee37956b
Madeline Park <madelinepark@lbl.gov>
2025-07-21 16:56:40 -0700

    more tests

--- client/castApp/src/testAddExcludePattern.c
+++ client/castApp/src/testAddExcludePattern.c
@@ -20,14 +20,17 @@ static void testAddExcludePatternX(void)
     caster.onmsg = &testLog;
 
     int argc;
-    char *argvlist[3];
+    char *argvlist[6];
     argvlist[0] = "addReccasterExcludePattern";
 
     char *expectedPatterns[] =
     {
         "*_",
         "*__",
-        "*:Intrnl:*"
+        "*:Intrnl:*",
+        "*_internal",
+        "_*",
+        "*exclude_me"
     };
     int expectedNumPatterns = 0;
 
@@ -54,8 +57,6 @@ static void testAddExcludePatternX(void)
         testOk1(strcmp(caster.exclude_patterns[i], expectedPatterns[i]) == 0);
     }
 
-    // ^ 8 tests
-
     testDiag("Testing addReccasterExcludePattern with a duplicate pattern");
     argvlist[1] = "*_";
     argc = 2;
@@ -66,13 +67,27 @@ static void testAddExcludePatternX(void)
         testOk1(strcmp(caster.exclude_patterns[i], expectedPatterns[i]) == 0);
     }
 
+    testDiag("Testing addReccasterExcludePattern with a new and a duplicate");
+    argvlist[1] = "*_internal";
+    argvlist[2] = "*__";
+    argc = 3;
+    testOk1(caster.num_exclude_patterns==expectedNumPatterns);
+    addReccasterExcludePattern(&caster, argc, argvlist);
+    expectedNumPatterns++;
+    testOk1(caster.num_exclude_patterns==expectedNumPatterns);
+    for(i=0; i < expectedNumPatterns; i++) {
+        testOk1(strcmp(caster.exclude_patterns[i], expectedPatterns[i]) == 0);
+    }
+
+
+
     epicsEventSignal(caster.shutdownEvent);
     casterShutdown(&caster);
 }
 
 MAIN(testAddExcludePattern)
 {
-    testPlan(13);
+    testPlan(19);
     osiSockAttach();
     testAddExcludePatternX();
     osiSockRelease();
