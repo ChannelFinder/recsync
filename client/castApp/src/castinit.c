7ede2077fb6e2181926ea893a6d1415344daa684
Madeline Park <madelinepark@lbl.gov>
2025-08-13 10:20:38 -0700

    errlogMajor for malloc fail, free memory if strdup fail

--- client/castApp/src/castinit.c
+++ client/castApp/src/castinit.c
@@ -244,13 +244,14 @@ void addReccasterExcludePattern(caster_t* self, int argc, char **argv) {
             if (!dup) {
                 string_list_t *new = malloc(sizeof(string_list_t));
                 if (new == NULL) {
-                    errlogSevPrintf(errlogMinor, "malloc error for creating linked list node");
+                    errlogSevPrintf(errlogMajor, "malloc error for creating linked list node");
                     ret = 1;
                     break;
                 }
                 new->item_str = strdup(argv[i]);
                 if (new->item_str == NULL) {
                     errlogSevPrintf(errlogMinor, "strdup error for copying %s to new->item_str from addReccasterExcludePattern\n", argv[i]);
+                    free(new);  // frees if strdup fails
                     ret = 1;
                     break;
                 }
