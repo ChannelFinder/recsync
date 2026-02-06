28d472278860db98f34ac10e886d33990393b0d0
Sky Brewer <sky.brewer@ess.eu>
2026-02-06 12:49:05 +0100

    update type-checking block recast

--- server/recceiver/recast.py
+++ server/recceiver/recast.py
@@ -6,8 +6,10 @@ import secrets
 import struct
 import sys
 import time
-from collections.abc import Callable
-from typing import Any
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
 
 from twisted.internet import defer, protocol, reactor
 from twisted.protocols import stateful
